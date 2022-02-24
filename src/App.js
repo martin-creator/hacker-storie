5717230254a8f2e1d30b5ad68e0688a9c31bf7c0
martin-creator <53592131+martin-creator@users.noreply.github.com>
2022-02-24 17:14:13 +0300

    Lists in react

--- src/App.js
+++ src/App.js
@@ -1,17 +1,45 @@
-import * as React from 'react';
+import * as React from "react";
 
-function getTitle(title){
-  return title
-}
+const list = [
+  {
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
 function App() {
   return (
     <div>
-      <h1> Hellon {getTitle('React')}</h1>
-      <label htmlFor='search'>Search:</label>
-      <input id = "search" type="text" />
-      
-      
+      <h1> My Hacker Stories</h1>
+      <label htmlFor="search">Search:</label>
+      <input id="search" type="text" />
+      {/* This is how you comment in JSX */}
+
+      <hr />
+      <ul>
+        {list.map( (item) => {
+          return (
+          <li key = {item.objectID}>
+          <span><a href={item.url}>{item.title}</a></span>
+          <span>{item.author}</span>
+          <span>{item.num_comments}</span>
+          <span>{item.points}</span>
+          
+          </li>);
+        })}
+      </ul>
     </div>
   );
 }
