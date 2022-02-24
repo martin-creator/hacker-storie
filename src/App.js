6611e873143f2aa500402ff62c509b439b0ca72c
martin-creator <53592131+martin-creator@users.noreply.github.com>
2022-02-24 18:10:19 +0300

    React props

--- src/App.js
+++ src/App.js
@@ -4,7 +4,26 @@ import Search from "./components/search";
 
 
 
-function App() {
+const App = () =>{
+ 
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
   return (
     <div>
       <h1> My Hacker Stories</h1>
@@ -13,7 +32,7 @@ function App() {
 
       <hr />
 
-      <List />
+      <List list = {stories} />
       
     </div>
   );
--- src/components/list.js
+++ src/components/list.js
@@ -1,36 +1,21 @@
- 
- const list = [
-    {
-      title: "React",
-      url: "https://reactjs.org/",
-      author: "Jordan Walke",
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-    },
-    {
-      title: "Redux",
-      url: "https://redux.js.org/",
-      author: "Dan Abramov, Andrew Clark",
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-    },
-  ];
- 
- function List (){
-     return(<ul>
-        {list.map( (item) => {
-          return (
-          <li key = {item.objectID}>
-          <span><a href={item.url}>{item.title}</a></span>
-          <span>{item.author}</span>
-          <span>{item.num_comments}</span>
-          <span>{item.points}</span>
-          
-          </li>);
-        })}
-      </ul>)   
- }
+const List = (props) => (
+  <ul>
+    {props.list.map((item) => (
+      <Item key={item.objectID} item={item} />
+    ))}
+  </ul>
+);
 
- export default List
+const Item = (props) => (
+    <li>
+    <span>
+      <a href={props.item.url}>{props.item.title}</a>
+    </span>
+    <span>{props.item.author}</span>
+    <span>{props.item.num_comments}</span>
+    <span>{props.item.points}</span>
+  </li>
+)
+    
+
+export default List;
